a2312fea66070e35f656657d35181ec4b26c7614
Splasher-69 <splasher69here@gmail.com>
2025-03-25 23:36:49 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "10396473"))
+    API_HASH = os.environ.get("API_HASH", "a9a9e84ef6ab7cfa4c5a5e4b758a786a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7592033056:AAHLxJFBkl_pt45yIwYG-cMJsB3Jv72iKh0")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "test_2_69_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
